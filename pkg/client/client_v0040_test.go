57c826d2e2d07baf9b91f49d97e517e8fd94c3c8
Skyler Malinowski <skyler@schedmd.com>
2025-05-13 11:59:12 -0400

    test: refactor api tests into their own test files

---
+++ pkg/client/client_v0040_test.go
@@ -0,0 +1,333 @@
+// SPDX-FileCopyrightText: Copyright (C) SchedMD LLC.
+// SPDX-License-Identifier: Apache-2.0
+
+package client
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/utils/ptr"
+
+	v0040 "github.com/SlinkyProject/slurm-client/api/v0040"
+	"github.com/SlinkyProject/slurm-client/pkg/object"
+	"github.com/SlinkyProject/slurm-client/pkg/types"
+)
+
+var _ = Describe("Client", func() {
+	const testTimeout = 30 * time.Second
+	const comment = "v0040"
+	var cfg *Config
+
+	BeforeEach(func() {
+		cfg = &Config{
+			Server:    restapiServer,
+			AuthToken: slurmJwt,
+		}
+	})
+
+	////////////////////////////////////////////////////////////////////////////
+
+	Describe("V0040ControllerPing", func() {
+		var cl Client
+
+		BeforeEach(func() {
+			var err error
+			cl, err = NewClient(cfg)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cl).NotTo(BeNil())
+
+			go cl.Start(context.TODO())
+
+			DeferCleanup(func() {
+				cl.Stop()
+			})
+		})
+
+		Context("Get", func() {
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("fetching non-existent object")
+				obj := &types.V0040ControllerPing{}
+				key := object.ObjectKey("slurmctld")
+				err := cl.Get(ctx, key, obj)
+				Expect(err).NotTo(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("List", func() {
+			It("should return a non-empty list ", func(ctx SpecContext) {
+				By("listing all objects")
+				list := &types.V0040ControllerPingList{}
+				err := cl.List(ctx, list)
+				Expect(err).NotTo(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+	})
+
+	Describe("V0040JobInfo", func() {
+		var cl Client
+		var req v0040.V0040JobSubmitReq
+
+		BeforeEach(func() {
+			var err error
+			cl, err = NewClient(cfg)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cl).NotTo(BeNil())
+
+			go cl.Start(context.TODO())
+
+			DeferCleanup(func() {
+				cl.Stop()
+			})
+		})
+
+		BeforeEach(func() {
+			req = v0040.V0040JobSubmitReq{
+				Job: &v0040.V0040JobDescMsg{
+					Environment: &v0040.V0040StringArray{
+						"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin",
+					},
+					CurrentWorkingDirectory: ptr.To("/tmp"),
+					Script:                  ptr.To("#!/usr/bin/bash\nexit 0"),
+				},
+			}
+		})
+
+		Context("Create", func() {
+			It("should create a new object", func(ctx SpecContext) {
+				By("creating the object")
+				obj := &types.V0040JobInfo{}
+				err := cl.Create(ctx, obj, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				actual := &types.V0040JobInfo{}
+				err = cl.Get(ctx, obj.GetKey(), actual)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("writing the result back to the go struct")
+				Expect(equality.Semantic.DeepEqual(obj, actual)).To(BeTrue())
+			}, SpecTimeout(testTimeout))
+			It("should fail if the object request is invalid", func(ctx SpecContext) {
+				By("creating the object")
+				obj := &types.V0040JobInfo{}
+				req := v0040.V0040JobSubmitReq{}
+				err := cl.Create(ctx, obj, req)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("Delete", func() {
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("deleting the object")
+				obj := &types.V0040JobInfo{V0040JobInfo: v0040.V0040JobInfo{JobId: ptr.To[int32](0)}}
+				err := cl.Delete(ctx, obj)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should delete a new object", func(ctx SpecContext) {
+				By("initially creating an object")
+				obj := &types.V0040JobInfo{}
+				err := cl.Create(ctx, obj, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("deleting the object")
+				err = cl.Delete(ctx, obj)
+				Expect(err).NotTo(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("Update", func() {
+			updateReq := v0040.V0040JobDescMsg{
+				Comment: ptr.To(comment),
+			}
+
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("update the object")
+				obj := &types.V0040JobInfo{V0040JobInfo: v0040.V0040JobInfo{JobId: ptr.To[int32](0)}}
+				err := cl.Update(ctx, obj, updateReq)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should update the existing object", func(ctx SpecContext) {
+				By("creating the object")
+				obj := &types.V0040JobInfo{}
+				err := cl.Create(ctx, obj, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("update the object")
+				err = cl.Update(ctx, obj, updateReq)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("validating the object field was updated")
+				Expect(equality.Semantic.DeepEqual(obj.Comment, updateReq.Comment)).To(BeTrue())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("Get", func() {
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("fetching non-existent object")
+				obj := &types.V0040JobInfo{}
+				key := object.ObjectKey("0")
+				err := cl.Get(ctx, key, obj)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should fetch an existing object for a go struct", func(ctx SpecContext) {
+				By("initially creating an object")
+				obj := &types.V0040JobInfo{}
+				err := cl.Create(ctx, obj, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("fetching the created the object")
+				actual := &types.V0040JobInfo{}
+				err = cl.Get(ctx, obj.GetKey(), actual)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("validating the fetched object equals the created one")
+				Expect(equality.Semantic.DeepEqual(obj, actual)).To(BeTrue())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("List", func() {
+			It("should return a list", func(ctx SpecContext) {
+				By("listing all objects")
+				list := &types.V0040JobInfoList{}
+				err := cl.List(ctx, list)
+				Expect(err).NotTo(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should return a non-empty list ", func(ctx SpecContext) {
+				By("initially creating an object")
+				obj := &types.V0040JobInfo{}
+				err := cl.Create(ctx, obj, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("listing all objects")
+				list := &types.V0040JobInfoList{}
+				err = cl.List(ctx, list)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("validating no objects are returned")
+				Expect(list.Items).NotTo(BeEmpty())
+			}, SpecTimeout(testTimeout))
+		})
+	})
+
+	Describe("V0040Node", func() {
+		var cl Client
+
+		BeforeEach(func() {
+			var err error
+			cl, err = NewClient(cfg)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cl).NotTo(BeNil())
+
+			go cl.Start(context.TODO())
+
+			DeferCleanup(func() {
+				cl.Stop()
+			})
+		})
+
+		Context("Delete", func() {
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("deleting the object")
+				obj := &types.V0040Node{V0040Node: v0040.V0040Node{Name: ptr.To("")}}
+				err := cl.Delete(ctx, obj)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("Update", func() {
+			req := v0040.V0040UpdateNodeMsg{
+				Comment: ptr.To(comment),
+			}
+
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("update the object")
+				obj := &types.V0040Node{V0040Node: v0040.V0040Node{Name: ptr.To("")}}
+				err := cl.Update(ctx, obj, req)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should update the existing object", func(ctx SpecContext) {
+				By("update the object")
+				obj := &types.V0040Node{V0040Node: v0040.V0040Node{Name: ptr.To("slurmd")}}
+				err := cl.Update(ctx, obj, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("validating the object field was updated")
+				Expect(equality.Semantic.DeepEqual(obj.Comment, req.Comment)).To(BeTrue())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("Get", func() {
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("fetching non-existent object")
+				obj := &types.V0040Node{V0040Node: v0040.V0040Node{Name: ptr.To("")}}
+				actual := &types.V0040Node{}
+				err := cl.Get(ctx, obj.GetKey(), actual)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should return existing object", func(ctx SpecContext) {
+				By("fetching existent object")
+				obj := &types.V0040Node{V0040Node: v0040.V0040Node{Name: ptr.To("slurmd")}}
+				actual := &types.V0040Node{}
+				err := cl.Get(ctx, obj.GetKey(), actual)
+				Expect(err).NotTo(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("List", func() {
+			It("should return a list", func(ctx SpecContext) {
+				By("listing all objects")
+				list := &types.V0040NodeList{}
+				err := cl.List(ctx, list)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(list.Items).NotTo(BeEmpty())
+			}, SpecTimeout(testTimeout))
+		})
+	})
+
+	Describe("V0040PartitionInfo", func() {
+		var cl Client
+
+		BeforeEach(func() {
+			var err error
+			cl, err = NewClient(cfg)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cl).NotTo(BeNil())
+
+			go cl.Start(context.TODO())
+
+			DeferCleanup(func() {
+				cl.Stop()
+			})
+		})
+
+		Context("Get", func() {
+			It("should fail if the object does not exist", func(ctx SpecContext) {
+				By("fetching non-existent object")
+				obj := &types.V0040PartitionInfo{V0040PartitionInfo: v0040.V0040PartitionInfo{Name: ptr.To("")}}
+				actual := &types.V0040PartitionInfo{}
+				err := cl.Get(ctx, obj.GetKey(), actual)
+				Expect(err).To(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+			It("should return existing object", func(ctx SpecContext) {
+				By("fetching existent object")
+				obj := &types.V0040PartitionInfo{V0040PartitionInfo: v0040.V0040PartitionInfo{Name: ptr.To("all")}}
+				actual := &types.V0040PartitionInfo{}
+				err := cl.Get(ctx, obj.GetKey(), actual)
+				Expect(err).NotTo(HaveOccurred())
+			}, SpecTimeout(testTimeout))
+		})
+
+		Context("List", func() {
+			It("should return a list", func(ctx SpecContext) {
+				By("listing all objects")
+				list := &types.V0040PartitionInfoList{}
+				err := cl.List(ctx, list)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(list.Items).NotTo(BeEmpty())
+			}, SpecTimeout(testTimeout))
+		})
+	})
+})
