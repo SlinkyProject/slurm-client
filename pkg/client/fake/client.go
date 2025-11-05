fb35e0cee6071948362c4a3f3d6f8e32636cf463
Skyler Malinowski <skyler@schedmd.com>
2025-11-05 11:15:59 -0500

    refactor: reorganize switch case into blocks
    Continuation of commit ca6579d0.

--- pkg/client/fake/client.go
+++ pkg/client/fake/client.go
@@ -114,51 +114,62 @@ func (c *fakeClient) Get(ctx context.Context, key object.ObjectKey, obj object.O
 		return errors.New(http.StatusText(http.StatusNotFound))
 	}
 	switch o := obj.(type) {
+	/////////////////////////////////////////////////////////////////////////////////
+
 	case *types.V0041ControllerPing:
 		cache := entry.(*types.V0041ControllerPing)
-		*o = *cache
-	case *types.V0042ControllerPing:
-		cache := entry.(*types.V0042ControllerPing)
-		*o = *cache
-	case *types.V0043ControllerPing:
-		cache := entry.(*types.V0043ControllerPing)
 		*o = *cache
 	case *types.V0041JobInfo:
 		cache := entry.(*types.V0041JobInfo)
 		*o = *cache
-	case *types.V0042JobInfo:
-		cache := entry.(*types.V0042JobInfo)
-		*o = *cache
-	case *types.V0043JobInfo:
-		cache := entry.(*types.V0043JobInfo)
-		*o = *cache
 	case *types.V0041Node:
 		cache := entry.(*types.V0041Node)
-		*o = *cache
-	case *types.V0042Node:
-		cache := entry.(*types.V0042Node)
-		*o = *cache
-	case *types.V0043Node:
-		cache := entry.(*types.V0043Node)
 		*o = *cache
 	case *types.V0041PartitionInfo:
 		cache := entry.(*types.V0041PartitionInfo)
 		*o = *cache
-	case *types.V0042PartitionInfo:
-		cache := entry.(*types.V0042PartitionInfo)
-		*o = *cache
-	case *types.V0043PartitionInfo:
-		cache := entry.(*types.V0043PartitionInfo)
-		*o = *cache
 	case *types.V0041Stats:
 		cache := entry.(*types.V0041Stats)
+		*o = *cache
+
+	/////////////////////////////////////////////////////////////////////////////////
+
+	case *types.V0042ControllerPing:
+		cache := entry.(*types.V0042ControllerPing)
+		*o = *cache
+	case *types.V0042JobInfo:
+		cache := entry.(*types.V0042JobInfo)
+		*o = *cache
+	case *types.V0042Node:
+		cache := entry.(*types.V0042Node)
+		*o = *cache
+	case *types.V0042PartitionInfo:
+		cache := entry.(*types.V0042PartitionInfo)
 		*o = *cache
 	case *types.V0042Stats:
 		cache := entry.(*types.V0042Stats)
 		*o = *cache
+
+	/////////////////////////////////////////////////////////////////////////////////
+
+	case *types.V0043ControllerPing:
+		cache := entry.(*types.V0043ControllerPing)
+		*o = *cache
+	case *types.V0043JobInfo:
+		cache := entry.(*types.V0043JobInfo)
+		*o = *cache
+	case *types.V0043Node:
+		cache := entry.(*types.V0043Node)
+		*o = *cache
+	case *types.V0043PartitionInfo:
+		cache := entry.(*types.V0043PartitionInfo)
+		*o = *cache
 	case *types.V0043Stats:
 		cache := entry.(*types.V0043Stats)
 		*o = *cache
+
+	/////////////////////////////////////////////////////////////////////////////////
+
 	default:
 		return errors.New(http.StatusText(http.StatusNotImplemented))
 	}
